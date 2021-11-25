6c9411c2a07e7fb9179d4dafdf1a88541363a9a8
Sandra Schuhmacher <coding@voiding-warranties.de>
2021-11-26 00:26:56 +0100

    added Client

--- YuliRox.gRPC.Client/Program.cs
+++ YuliRox.gRPC.Client/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using YuliRox.gRPC;
+
+namespace YuliRox.gRPC.Client
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            using var channel = GrpcChannel.ForAddress("https://localhost:7260");
+            var client = new RecordingService.RecordingServiceClient(channel);
+
+            var rc = await client.StartRecordingAsync(new StartRequest() { Id = 3 });
+            Console.WriteLine("Start request " + (rc.Rc == true ? "successful " : "failed"));
+
+            var rc2 = await client.StopRecordingAsync(new StopRequest() { Id = 3 });
+            Console.WriteLine("Stop request " + (rc2.Rc == true ? "successful " : "failed"));
+
+            var status = await client.GetRecordingStatusAsync(new StatusRequest());
+            Console.WriteLine(status);
+
+            var id = await client.GetCurrentRecordingIdAsync(new CurrentRecordingIdRequest());
+            Console.WriteLine(id.Id);
+
+            Console.WriteLine("Shutting down");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
